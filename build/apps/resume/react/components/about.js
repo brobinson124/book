0907502fce43b217a34bb774336ec29cf0a5071d
Brooke Robinson <brooke.robinson@colorado.edu>
2016-02-07 22:40:17 -0700

    finished week 4

--- build/apps/resume/react/components/about.js
+++ build/apps/resume/react/components/about.js
@@ -1,13 +1,36 @@
+
 MyComponents.About = React.createClass({
 
   render: function() {
     return (
-      <div className="card">
+      <div className="card blue">
         <div className="card-content">
-        TODO: This is a component to display the basic
-        information about myself such as name, profile photo,
-        major, birth place, ... etc.
+
+          <div className="row">
+          <div className="col s3">
+          <div className="row about-me-face">
+
+        <img src="profile_pic.jpg" height="200" width="200"/>
         </div>
+        </div>
+        <div className="col s5">
+
+        <ul>
+              <li><b>Name:</b> Brooke Robinson</li>
+              <li><b>Major:</b> Computer Science B.S.</li>
+              <li><b>Focus:</b> Human Centered Computing</li>
+              <li><b>Year:</b> Junior</li>
+              <li><b>Birth Place:</b> Alaska</li>
+              <li><b>Favorite Color:</b> Blue</li>
+              <li><b>Github URL:</b> <a href="https://github.com/brobinson124">www.github.com/brobinson124</a></li>
+            </ul>
+        </div>
+
+        <div className="col s2">
+        </div>
+
+      </div>
+      </div>
       </div>
     );
   }
