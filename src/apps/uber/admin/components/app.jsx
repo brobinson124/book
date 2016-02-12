07387a85771bf589c8e2d86bc9f0a625f0ca7139
aokeson <Alexandra.Okeson@colorado.edu>
2016-02-12 08:43:04 -0700

    Admin maps and nav bar

--- src/apps/uber/admin/components/app.jsx
+++ src/apps/uber/admin/components/app.jsx
@@ -1,12 +1,16 @@
 class App extends React.Component {
   render(){
     return <div>
-      <MyComponents.UserMap users={this.props.data.users}/>
+      <div className="card">
+        <MyComponents.UserMap users={this.props.data.users} center={this.props.data.center}/>
+      </div>
       <MyComponents.UserList users={this.props.data.users}/>
-      <MyComponents.ProviderMap users={this.props.data.providers}/>
+      <div className="card">
+        <MyComponents.ProviderMap providers={this.props.data.providers} center={this.props.data.center}/>
+      </div>
       <MyComponents.ProviderList providers={this.props.data.providers}/>
-    </div>
+    </div>;
   }
 }
 
-MyComponents.App = App
+MyComponents.App = App;
--- src/apps/uber/admin/components/provider-map.jsx
+++ src/apps/uber/admin/components/provider-map.jsx
@@ -1,11 +1,34 @@
+
+const {Map, Marker, CircleMarker, Popup, TileLayer, MapLayer}  = window.ReactLeaflet
+
 class ProviderMap extends React.Component {
   render(){
-    return <div>
-      <div>ToDo: ToDo: Show All Providers on a Map
-        <pre>{JSON.stringify(this.props.users)}</pre>
-      </div>
-    </div>
+  	console.log(this.props.providers)
+    const providers = this.props.providers
+    const providerElements = _.map(providers, function(p,i){
+      return <Marker position={p.pos} key={i}>
+        <Popup>
+          <span>{p.username}</span>
+          <span>{p.status}</span>
+        </Popup>
+      </Marker>
+    })
+
+    // Note: .bind(this) is important for the handler function's 'this'
+    // pointer to refer to this MapView instance
+
+    var token = 'pk.eyJ1Ijoibmljb3QiLCJhIjoiY2lqdnkzbGQyMGRqY3VjbTVwbDNyOGcxaiJ9.-tW3kDrfp15nLw82zErsjg'
+    const mapurl = 'https://api.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}.png?access_token=' + token
+    return  <Map center={this.props.center}
+          zoom={14}>
+        <TileLayer
+          url={mapurl}
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+        {providerElements}
+      </Map>
   }
 }
 
 MyComponents.ProviderMap = ProviderMap
+
