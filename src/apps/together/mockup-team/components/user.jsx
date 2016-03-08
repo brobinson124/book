6d1d60312e38685a5476252bbbe0afdd36e9fd0f
Brooke Robinson <brooke.robinson@colorado.edu>
2016-03-07 18:05:10 -0700

    fixed minor syntax error

--- src/apps/together/mockup-team/components/user.jsx
+++ src/apps/together/mockup-team/components/user.jsx
@@ -9,7 +9,7 @@ class User extends React.Component {
                 <li class="active"><a href="index.html">Home</a></li>
                 <li><a href="signup.html">Sign Up!</a></li>
                 <li><a href="newscore.html">New Score</a></li>
-                <li><a href="friend.html"t with is the enjoyment of creating something new, and well >Find Friends</a></li>
+                <li><a href="friend.html">Find Friends</a></li>
                 <li><a href={"https://github.com/" + u}>{u}</a></li>
                 <li><a className="right" href="#" onClick={this.props.logoutAction}>Logout</a></li>
             </ul>;
@@ -18,7 +18,7 @@ class User extends React.Component {
                 <li class="active"><a href="index.html">Home</a></li>
                 <li><a href="signup.html">Sign Up!</a></li>
                 <li><a href="newscore.html">New Score</a></li>
-                <li><a href="friend.html"t with is the enjoyment of creating something new, and well >Find Friends</a></li>
+                <li><a href="friend.html">Find Friends</a></li>
                 <li><a href="#" onClick={this.props.loginAction}>Login</a></li>
                 <li className ="active"><a href="index.html">Client</a></li>
                 <li><a href="../admin/index.html">Admin</a></li>
@@ -28,4 +28,3 @@ class User extends React.Component {
 
 }
 MyComponents.User = User;
-
